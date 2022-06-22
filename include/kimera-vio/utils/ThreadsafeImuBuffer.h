c7f5e217c2d69935e7e21f7dc8927e563b9353d7
HyeonJaeGil <now9728@gmail.com>
2022-06-22 21:37:13 +0900

    add utils (not finished)

---
+++ include/kimera-vio/utils/ThreadsafeImuBuffer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace VIO {
+
+    namespace utils {
+
+        class ThreadsafeImuBuffer {
+
+        //    explicit ThreadsafeImuBuffer(const Timestamp& buffer_length_ns)
+        //     : buffer_(buffer_length_ns), shutdown_(false) {}
+
+        //     ~ThreadsafeImuBuffer() { shutdown(); }   
+
+        //     inline void shutdown();
+            
+        /* temporary constructor (only for complilation) */
+        public:
+
+        ThreadsafeImuBuffer(int temp) {}
+
+        };
+
+    }
+}
