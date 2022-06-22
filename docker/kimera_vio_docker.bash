2003c65b6a546c4150d23d9636cc653b97a50abb
HyeonJaeGil <now9728@gmail.com>
2022-06-22 18:30:18 +0900

    add dockerfile

---
+++ docker/kimera_vio_docker.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Allow X server connection
+xhost +local:root
+docker run -it --rm \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="/:/mydata" \
+    hyeonjaegil/kimera_vio:test
+# Disallow X server connection
+xhost -local:root
