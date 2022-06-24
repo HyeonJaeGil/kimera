0c3050ab53d4bdc70b7874f111b6968eca8d9d1d
HyeonJaeGil <now9728@kaist.ac.kr>
2022-06-25 03:48:00 +0900

    skeleton code from kimera-vio (not complied yet)

--- examples/KimeraVIO.cpp
+++ examples/KimeraVIO.cpp
@@ -3,9 +3,58 @@
 #include <utility>
 
 #include "kimera-vio/pipeline/Pipeline-definitions.h"
+#include "kimera-vio/dataprovider/DataProviderInterface.h"
+#include "kimera-vio/common/vio_types.h"
 
 int main(int argc, char* argv[]){
     
     VIO::VioParams vio_params("folder/path");
     vio_params.print();
+
+    VIO::DataProviderInterface::Ptr dataset_parser = 
+        VIO::make_unique<VIO::MonoEurocDataProvider>(vio_params);
+    
+    VIO::Pipeline::Ptr vio_pipeline;
+    vio_pipeline = VIO::make_unique<VIO::MonoImuPipeline>(vio_params);
+
+    vio_pipeline->registerShutdownCallback(
+        std::bind(&VIO::DataProviderInterface::shutdown, dataset_parser));
+
+    // Register callback to vio pipeline.
+    // dataset_parser가 읽어오는 measurement를 
+    // VIO::Pipeline::fillSingleImuQueue 함수에 넣도록 파이프라인 구축.
+    dataset_parser->registerImuSingleCallback(
+        std::bind(&VIO::Pipeline::fillSingleImuQueue,
+                vio_pipeline,
+                std::placeholders::_1));
+    
+    // We use blocking variants to avoid overgrowing the input queues (use
+    // the non-blocking versions with real sensor streams)
+    dataset_parser->registerLeftFrameCallback(
+        std::bind(&VIO::Pipeline::fillLeftFrameQueue,
+                vio_pipeline,
+                std::placeholders::_1));
+
+    auto handle = std::async(std::launch::async,
+                                &VIO::DataProviderInterface::spin,
+                                dataset_parser);
+    auto handle_pipeline =
+        std::async(std::launch::async,
+                    &VIO::Pipeline::spin,
+                    vio_pipeline);
+    auto handle_shutdown =
+        std::async(std::launch::async,
+                    &VIO::Pipeline::shutdownWhenFinished,
+                    vio_pipeline,
+                    500,
+                    true);
+    
+    vio_pipeline->spinViz();
+    
+    bool is_pipeline_successful = false;
+    is_pipeline_successful = !handle.get();
+    handle_shutdown.get();
+    handle_pipeline.get();
+
+
 }
---
+++ include/kimera-vio/pipeline/Pipeline.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <thread>
+
+#include "kimera-vio/pipeline/Pipeline-definitions.h"
+#include "kimera-vio/utils/Macros.h"
+
+namespace VIO
+{
+    class Pipeline{
+    public:
+        KIMERA_POINTER_TYPEDEFS(Pipeline);
+        KIMERA_DELETE_COPY_CONSTRUCTORS(Pipeline);
+        // EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        
+    public:
+        Pipeline(const VioParams& params);
+        virtual ~Pipeline();
+
+    public:
+        inline void fillLeftFrameQueue(Frame::UniquePtr left_frame) {}
+        inline void fillLeftFrameQueueBlockingIfFull(Frame::UniquePtr left_frame) {}
+        inline void fillSingleImuQueue(const ImuMeasurement& imu_measurement) {}
+        inline void fillMultiImuQueue(const ImuMeasurements& imu_measurements) {}
+        
+        virtual bool spin();
+        virtual bool spinViz();
+        virtual std::string printStatus() const;
+        virtual bool spin();
+        virtual bool spinViz();
+        virtual std::string printStatus() const;
+        virtual bool hasFinished() const;
+        virtual bool shutdownWhenFinished(const int& sleep_time_ms = 500,
+                                        const bool& print_stats = false);
+        virtual void shutdown();
+        inline bool isShutdown() const { return shutdown_; }
+        virtual void resume();
+        virtual void registerShutdownCallback
+            (const ShutdownPipelineCallback& callback) {
+                shutdown_pipeline_cb_ = callback;
+        }
+        inline std::string printStatistics() const {
+            return utils::Statistics::Print();
+        }
+
+    protected:
+
+        BackendParams::ConstPtr backend_params_;
+        FrontendParams frontend_params_;
+        ImuParams imu_params_;
+        bool parallel_run_;
+        virtual void spinOnce(FrontendInputPacketBase::UniquePtr input);
+        virtual void spinSequential();
+        virtual void setDeterministicPipeline() const { srand(0); }
+        virtual void launchThreads();
+        virtual void stopThreads();
+        virtual void joinThreads();
+
+
+    protected:
+        
+        MonoDataProviderModule::UniquePtr data_provider_module_;
+        VisionImuFrontendModule::UniquePtr vio_frontend_module_;
+        VioBackendModule::UniquePtr vio_backend_module_;
+        MesherModule::UniquePtr mesher_module_;
+        LcdModule::UniquePtr lcd_module_;
+        VisualizerModule::UniquePtr visualizer_module_;
+        DisplayModule::UniquePtr display_module_;
+        
+        VisionImuFrontendModule::InputQueue frontend_input_queue_;
+        VioBackendModule::InputQueue backend_input_queue_;
+        DisplayModule::InputQueue display_input_queue_;
+
+        std::unique_ptr<std::thread> frontend_thread_ = {nullptr};
+        std::unique_ptr<std::thread> backend_thread_ = {nullptr};
+        std::unique_ptr<std::thread> mesher_thread_ = {nullptr};
+        std::unique_ptr<std::thread> lcd_thread_ = {nullptr};
+        std::unique_ptr<std::thread> visualizer_thread_ = {nullptr};
+
+    };
+
+
+}
